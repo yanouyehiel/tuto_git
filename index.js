4a819a8c8c0055942290298f03eb0cbda025060c
JunsTeg <teguem@icloud.com>
2024-08-02 12:02:27 +0100

    Ajout du code

---
+++ index.js
@@ -0,0 +1,9 @@
+function addition(a, b) {
+    return a + b;
+  }
+  
+  // Exemple d'utilisation
+  const num1 = 5;
+  const num2 = 3;
+  const result = addition(num1, num2);
+  console.log(`La somme de ${num1} et ${num2} est ${result}`);
